1f93c9913a39584d95eee039a972d91a25b7521a
Maui Bautista <mherdy_2@hotmail.com>
2023-05-31 21:04:39 -0700

    Party logs

--- script.js
+++ script.js
@@ -52,51 +52,37 @@ const deleteParty = async (id) => {
 // render a single party by id
 const renderSinglePartyById = async (id) => {
   try {
-    // fetch party details from server
     const party = await getPartyById(id);
-
-    // GET - /api/workshop/guests/party/:partyId - get guests by party id
     const guestsResponse = await fetch(`${GUESTS_API_URL}/party/${id}`);
     const guests = await guestsResponse.json();
-
-    // GET - /api/workshop/rsvps/party/:partyId - get RSVPs by partyId
     const rsvpsResponse = await fetch(`${RSVPS_API_URL}/party/${id}`);
     const rsvps = await rsvpsResponse.json();
 
-    // GET - get all gifts by party id - /api/workshop/parties/gifts/:partyId -BUGGY?
-    // const giftsResponse = await fetch(`${PARTIES_API_URL}/party/gifts/${id}`);
-    // const gifts = await giftsResponse.json();
-
-    // create new HTML element to display party details
     const partyDetailsElement = document.createElement("div");
     partyDetailsElement.classList.add("party-details");
     partyDetailsElement.innerHTML = `
-            <h2>${party.title}</h2>
-            <p>${party.event}</p>
-            <p>${party.city}</p>
-            <p>${party.state}</p>
-            <p>${party.country}</p>
-            <h3>Guests:</h3>
-            <ul>
-            ${guests
-              .map(
-                (guest, index) => `
+      <h2>${party.name}</h2>
+      <p>${party.description}</p>
+      <p>${party.date}</p>
+      <p>${party.time}</p>
+      <p>${party.location}</p>
+      <h3>Guests:</h3>
+      <ul>
+        ${guests
+          .map(
+            (guest, index) => `
               <li>
                 <div>${guest.name}</div>
                 <div>${rsvps[index].status}</div>
               </li>
             `
-              )
-              .join("")}
-          </ul>
-          
-
-
-            <button class="close-button">Close</button>
-        `;
+          )
+          .join("")}
+      </ul>
+      <button class="close-button">Close</button>
+    `;
     partyContainer.appendChild(partyDetailsElement);
 
-    // add event listener to close button
     const closeButton = partyDetailsElement.querySelector(".close-button");
     closeButton.addEventListener("click", () => {
       partyDetailsElement.remove();
